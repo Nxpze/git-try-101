4eb39851827471193ce59bcc5bb763c9c5022b99
Nxpze <nnnopphara.t2549@gmail.com>
2026-08-17 17:45:13 +0700

    def new function

--- frist try.py
+++ frist try.py
@@ -2,3 +2,9 @@ print(' Hello, World!')
 nxpze = "นพรัตน์ ศรีวงษา"
 
 print(f"Hello, {nxpze}!")
+
+def say_goodnight(N):
+    while N <= 50:
+        print(f"good night {nxpze}")
+        N += 1
+say_goodnight(5)
