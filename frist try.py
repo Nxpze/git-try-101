85fead19db6a6c31a2dfbc3e70a9af72b7d3be15
Nxpze <nnnopphara.t2549@gmail.com>
2026-08-19 23:48:30 +0700

    add age cal function

--- frist try.py
+++ frist try.py
@@ -1,10 +1,28 @@
-print(' Hello, World!')
-nxpze = "นพรัตน์ ศรีวงษา"
+from datetime import date
+import random
 
-print(f"Hello, {nxpze}!")
 
-def say_goodnight(N):
-    while N <= 50:
-        print(f"good night {nxpze}")
-        N += 1
-say_goodnight(5)
+def age_calculator(year, month, day, name):
+    today = date.today()
+    birthdate = date(year, month, day)
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+if __name__ == "__main__":
+    try:
+        name = input("กรุณากรอกชื่อของคุณ: ")
+        year = int(input("กรุณากรอกปีเกิดของคุณ (คริสต์ศักราช) (YYYY): "))
+        month = int(input("กรุณากรอกเดือนเกิดของคุณ (MM): "))
+        day = int(input("กรุณากรอกวันเกิดของคุณ (DD): "))
+
+        age = age_calculator(year, month, day, name)
+        if age >= 20:
+            print(f"คุณยังบรรลุนิติภาวะแล้ว {name} อายุ {age} ปี")
+        else:
+            print(f"คุณยังไม่บรรลุนิติภาวะ {name} อายุ {age} ปี")
+    except ValueError:
+        print("กรุณากรอกข้อมูลให้ถูกต้อง")
+        exit()
+
+
+
